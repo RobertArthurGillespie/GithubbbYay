b63e4fcd5b596f74d975602aec52e4d854edf36d
wayloft <82181607+wayloft@users.noreply.github.com>
2021-04-08 16:50:32 -0500

    Object Detection Script
    Detects Objects

---
+++ Assets/DetectObjectHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectObjectHandler : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitObject;
+            if(Physics.Raycast(ray, out hitObject))
+            {
+                print(string.Format("You have clicked on {0}", hitObject));
+            }
+        }
+    }
+}
