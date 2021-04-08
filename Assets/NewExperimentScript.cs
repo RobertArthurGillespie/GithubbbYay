d904746e90ba3eb73d40316052b16bba1a816ff8
DevonRLW <dtramirez54@yahoo.com>
2021-04-08 17:13:20 -0500

    Devons new script
    added to the script

--- Assets/NewExperimentScript.cs
+++ Assets/NewExperimentScript.cs
@@ -13,6 +13,6 @@ public class NewExperimentScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("This is the new thing");  
     }
 }
