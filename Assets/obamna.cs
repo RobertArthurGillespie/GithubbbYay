20cb0451943e2b487fa52e718bad56d99c936d67
FrostyBrain <57225090+FrostyBrain@users.noreply.github.com>
2021-04-08 16:52:09 -0500

    obamna
    SODA

---
+++ Assets/obamna.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obamna : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GET DAT PEPPUR OFF DERE, COME ON NOW");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
